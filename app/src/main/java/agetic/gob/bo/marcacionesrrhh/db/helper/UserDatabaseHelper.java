bc0c02bfa8744aa70ec7d9eb8ffbf2f858160914
Ramiro <rhuayller@agetic.gob.bo>
2016-07-21 09:43:06 -0400

    se agrego helper para usuario, funcion para agregar usuario a la bd

--- app/src/main/java/agetic/gob/bo/marcacionesrrhh/db/helper/UserDatabaseHelper.java
+++ app/src/main/java/agetic/gob/bo/marcacionesrrhh/db/helper/UserDatabaseHelper.java
@@ -1,8 +1,13 @@
 package agetic.gob.bo.marcacionesrrhh.db.helper;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import agetic.gob.bo.marcacionesrrhh.db.model.User;
 
 /**
  * Created by ramiro on 20-07-16.
@@ -18,31 +23,46 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_UID = "uid";
     private static final String COLUMN_PASSWORD = "password";
     private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_EMAILl = "email";
+    private static final String COLUMN_EMAIL = "email";
     private static final String COLUMN_POSITION = "position";
     //query: create table a (b text, c text);
-    private static final String CREATE_USER_TABLE_QUERY = "create table a (b text, c text);";
+    private static final String CREATE_USER_TABLE_QUERY = "CREATE TABLE " + TABLE_NAME +
+            " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+            COLUMN_TOKEN + " TEXT" +
+            COLUMN_UID + " TEXT" +
+            COLUMN_PASSWORD + " TEXT" +
+            COLUMN_NAME + " TEXT" +
+            COLUMN_EMAIL + " TEXT" +
+            COLUMN_POSITION + " TEXT" +
+            ");";
 
-
-/*    private int _id;
-    private String _token;
-    private String _uid;
-    private String _password;
-    private String _name;
-    private String _email;
-    private String _position;*/
-    
-    public UserDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public UserDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(CREATE_USER_TABLE_QUERY);
+        } catch (SQLException e) {
+            Log.d("LOG CREATE TABLE", e.getMessage());
+        }
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+    // add user to db
+    public void addUser(User user){
+        SQLiteDatabase db = this. getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TOKEN, user.get_token());
+        values.put(COLUMN_UID, user.get_uid());
+        values.put(COLUMN_PASSWORD, user.get_password());
+        values.put(COLUMN_NAME, user.get_name());
+        values.put(COLUMN_EMAIL, user.get_email());
+        values.put(COLUMN_POSITION, user.get_position());
 
     }
 }
