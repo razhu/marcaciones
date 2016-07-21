b9f8783a4506f6db9cdabdd1a8e54813c630c757
Ramiro <rhuayller@agetic.gob.bo>
2016-07-21 09:05:08 -0400

    construyendo la bd

--- app/src/main/java/agetic/gob/bo/marcacionesrrhh/db/helper/UserDatabaseHelper.java
+++ app/src/main/java/agetic/gob/bo/marcacionesrrhh/db/helper/UserDatabaseHelper.java
@@ -8,6 +8,22 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by ramiro on 20-07-16.
  */
 public class UserDatabaseHelper extends SQLiteOpenHelper {
+    //database
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Rrhh.db";
+    private static final String TABLE_NAME = "users";
+    //columns
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TOKEN = "token";
+    private static final String COLUMN_UID = "uid";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_EMAILl = "email";
+    private static final String COLUMN_POSITION = "position";
+    //query: create table a (b text, c text);
+    private static final String CREATE_USER_TABLE_QUERY = "create table a (b text, c text);";
+
+
 /*    private int _id;
     private String _token;
     private String _uid;
